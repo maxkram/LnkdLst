a1426ab9884c1cfd8207e54ff87eb6651221af83
maxkram <max.kramer@live.com>
2017-04-23 08:19:25 +0300

    first try

---
+++ src/ru/maxkramer/Album.java
@@ -0,0 +1,35 @@
+
+package ru.maxkramer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by maxkr on 4/23/2017.
+ */
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String title, double duration){
+        if(findSong(title) == null){
+            this.songs.add(new Song(title, duration));
+        }
+        return false;
+    }
+
+    private Song findSong(String title){
+         for(Song checkedSong: this.songs){
+             if(checkedSong.getTitle().equals(title)){
+                 return checkedSong;
+             }
+         }
+         return null;
+    }
+}
---
+++ src/ru/maxkramer/Main.java
@@ -0,0 +1,10 @@
+package ru.maxkramer;
+
+/**
+ * Created by maxkr on 4/23/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
